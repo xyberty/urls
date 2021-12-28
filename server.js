4766fecd0f2ff556dbbee4959be3b4dcaeed3f45
xyberty <cyberty@yandex.ru>
2021-12-28 12:35:17 +0100

    Update formatting

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ mongoose
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+
 app.get("/", async (req, res) => {
   const shortUrls = await ShortUrl.find();
   res.render("index", { shortUrls: shortUrls });
@@ -23,6 +24,7 @@ app.post("/shortUrls", async (req, res) => {
   await ShortUrl.create({ full: req.body.fullUrl });
   res.redirect("/");
 });
+
 app.get("/:shortUrl", async (req, res) => {
   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
   if (shortUrl == null) return res.sendStatus(404);
@@ -30,6 +32,7 @@ app.get("/:shortUrl", async (req, res) => {
   shortUrl.save();
   res.redirect(shortUrl.full);
 });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- shortUrl.js
+++ shortUrl.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose')
 const shortId = require('shortid')
+
+shortId.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_!')
+
 const shortUrlSchema = new mongoose.Schema({
     full: {
         type: String, 
         required: true
     },
-    short : {
-        type:String, 
+    short: {
+        type: String, 
         required: true,
         default: shortId.generate
     },
-    clicks:{
-        type:Number, 
+    clicks: {
+        type: Number, 
         required: true, 
         default: 0
     }
