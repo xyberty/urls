051f8e6077842188a9bd97d6d14a030cd1017d7f
Manish Sencha <manishsencha2001@gmail.com>
2022-09-19 21:02:00 +0530

    Deploy on netlify"

---
+++ app.js
@@ -0,0 +1,7 @@
+const app = require("./src/server")
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const ShortUrl = require("./shortUrl");
+const serverless = require("serverless-http")
+const path = require('path')
 require("dotenv").config();
+
 const app = express();
+const router = express.Router();
 
 mongoose
   .connect(process.env.DB_URL, {
@@ -14,23 +18,28 @@ mongoose
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
-app.get("/", async (req, res) => {
+
+router.get("/", async (req, res) => {
   const shortUrls = await ShortUrl.find();
   res.render("index", { shortUrls: shortUrls });
 });
 
-app.post("/shortUrls", async (req, res) => {
+router.post("/shortUrls", async (req, res) => {
   await ShortUrl.create({ full: req.body.fullUrl });
   res.redirect("/");
 });
-app.get("/:shortUrl", async (req, res) => {
+
+router.get("/:shortUrl", async (req, res) => {
   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
   if (shortUrl == null) return res.sendStatus(404);
   shortUrl.clicks++;
   shortUrl.save();
   res.redirect(shortUrl.full);
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+
+app.use('/.netlify/functions/server', router);
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')))
+
+modu1e.exports = app;
+module.exports.handler = serverless(app)
